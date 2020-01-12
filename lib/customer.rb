a82fc716c31171301005a5355e2cd0a3966d681a
arnie b <arnold.bwaila@gmail.com>
2020-01-12 15:26:01 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
+  class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def waiters
+   meals = Meal.all.select {|m| m.customer == self
+   meals.collect { |ml| ml.waiter}
+  end
 end
